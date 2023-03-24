40af0800c53fdb747d570a3004544f45725518c8
sergio <sergio.sejo@gmail.com>
2023-03-24 18:47:15 +0100

    creation structure

---
+++ src/components/ZaraPodcast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const ZaraPodcast = () => {
+	return (
+		<>
+			<Navbar brand="Podcaster" />
+		</>
+	);
+};
+
+export default ZaraPodcast;
