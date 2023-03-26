04a4232d27cfb70bb81329b5157d6d0d277a1cfa
sergio <sergio.sejo@gmail.com>
2023-03-26 15:28:20 +0200

    start with detail page

--- src/components/detailPodcast/DetailPodcast.js
+++ src/components/detailPodcast/DetailPodcast.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import podcastService from '../../services/podcastService';
 
 const DetailPodcast = () => {
-	return <div>DetailPodcast</div>;
+	const pdService = new podcastService();
+	const [detail, setDetail] = useState([]);
+	const { id } = useParams();
+	useEffect(() => {
+		const getDetail = async () => {
+			let result = await pdService.getPodcastDetail(id);
+			setDetail(result);
+		};
+		getDetail();
+	}, []);
+
+	return (
+		<div className="container" style={{ marginTop: '30px' }}>
+			<div className="row">
+				<div className="col-lg-3">
+					<div className="card" style={{ minWidth: '200px' }}>
+						<img
+							className="card-img-top"
+							alt="podcast"
+							src={detail.artworkUrl600}
+						/>
+						<div className="card-body text-center">
+							<h5 className="card-title">{detail.trackName}</h5>
+							<p className="card-text">by {detail.artistName}</p>
+							<hr />
+							<h6 className="card-title">Description:</h6>
+							<p className="card-text">{detail.artistName}</p>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-9">
+					<div className="card row">
+						<h3 className="episodes">Episodes: {detail.trackCount}</h3>
+					</div>
+					<div className="card row">
+						<div className="episodes">Tabla</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DetailPodcast;
--- src/services/podcastService.js
+++ src/services/podcastService.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const url =
 	'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
 
+const urlDetail = 'https://itunes.apple.com/lookup?id=';
+
 class podcastService {
 	get100PodcastsApple = async () => {
 		try {
@@ -10,6 +12,17 @@ class podcastService {
 			return res.data.feed.entry;
 		} catch (ex) {
 			console.log('podcastService.getPodcasts: ', ex);
+			return null;
+		}
+	};
+
+	getPodcastDetail = async (id) => {
+		try {
+			const res = await axios.get(`${urlDetail}${id}&country=no`);
+			console.log('res: ', res);
+			return res.data.results[0];
+		} catch (ex) {
+			console.log('podcastService.getPodcastDetail: ', ex);
 			return null;
 		}
 	};
